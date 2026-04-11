c7788e2730a537985faef9c33aa5a343f35996fa
pouu69 <pouu69@gmail.com>
2026-04-11 15:12:24 +0900

    remove cache workers

--- sw.js
+++ sw.js
@@ -1,38 +1,17 @@
-var CACHE_NAME = 'tamagoji-v5';
-var urlsToCache = [
-    '/',
-    '/index.html',
-    '/numbers.html',
-    '/app.min.js',
-    '/css/styles.css',
-    '/manifest.json'
-];
-
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            return cache.addAll(urlsToCache);
-        })
-    );
+self.addEventListener('install', function() {
     self.skipWaiting();
 });
 
 self.addEventListener('activate', function(e) {
     e.waitUntil(
-        caches.keys().then(function(names) {
-            return Promise.all(
-                names.filter(function(n) { return n !== CACHE_NAME; })
-                    .map(function(n) { return caches.delete(n); })
-            );
-        })
-    );
-    self.clients.claim();
-});
-
-self.addEventListener('fetch', function(e) {
-    e.respondWith(
-        caches.match(e.request).then(function(response) {
-            return response || fetch(e.request);
-        })
+        caches.keys()
+            .then(function(names) {
+                return Promise.all(names.map(function(n) { return caches.delete(n); }));
+            })
+            .then(function() { return self.registration.unregister(); })
+            .then(function() { return self.clients.matchAll(); })
+            .then(function(clients) {
+                clients.forEach(function(c) { c.navigate(c.url); });
+            })
     );
 });
